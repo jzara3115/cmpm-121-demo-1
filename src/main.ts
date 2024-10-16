87ca67643702bbbe7ade7a0ce5155d559a50a2ae
JC Zaragoza <jzara3115@gmail.com>
2024-10-15 22:07:07 -0700

    Step 1 - Button Made

--- src/main.ts
+++ src/main.ts
@@ -9,11 +9,11 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
-const mybutton = document.createElement('button');
-mybutton.textContent = '🍪';
+const mybutton = document.createElement("button");
+mybutton.textContent = "🍪";
 
-mybutton.addEventListener('click', () => {
-    //console.log('Button was clicked!');
+mybutton.addEventListener("click", () => {
+  //console.log('Button was clicked!');
 });
 
 app.append(mybutton);
