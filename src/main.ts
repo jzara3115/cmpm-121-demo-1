4241b2fe83c7206ba3c1e4d233637ad10ca196f8
JC Zaragoza <jzara3115@gmail.com>
2024-10-15 22:55:49 -0700

    Step 5 - Can use first upgrade button and enables and disables it when can be bought

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import "./style.css";
 const app: HTMLDivElement = document.querySelector("#app")!;
 
 let cookedNum: number = 0;
-const cookRate: number = 1;
+let cookRate: number = 1;
+
+const upgradeOneCost: number = 10;
 
 const gameName = "Flowers!!!";
 document.title = gameName;
@@ -14,17 +16,27 @@ app.append(header);
 
 const mybutton = document.createElement("button");
 const counter = document.createElement("div");
+const upgradeOne = document.createElement("button");
 
 mybutton.textContent = "🍪";
-counter.textContent = `Cookies Baked: ${cookedNum}`;
+counter.textContent = `Flowers Grown: ${cookedNum}`;
+upgradeOne.textContent = `A bee, allows for one more picked /s: ${upgradeOneCost}`;
 
 app.append(mybutton);
 app.append(counter);
+app.append(upgradeOne);
+
+upgradeOne.disabled = true;
 
 mybutton.addEventListener("click", () => {
   cookedNum++;
   console.log(cookedNum);
 });
+
+upgradeOne.addEventListener("click", () => {
+    cookRate++;
+    cookedNum = cookedNum - upgradeOneCost;
+  });
 
 function addRate() {
   cookedNum = cookRate + cookedNum;
@@ -34,6 +46,13 @@ setInterval(addRate, 1000);
 
 function updateCounter() {
   counter.textContent = `Cookies Baked: ${cookedNum}`;
+
+  if(cookedNum >= upgradeOneCost){
+    upgradeOne.disabled = false;
+} else {
+    upgradeOne.disabled = true;
+}
+
   requestAnimationFrame(updateCounter);
 }
 
