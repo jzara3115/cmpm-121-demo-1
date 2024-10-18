c4aaf696b89f84fe6f7360078bae1b972c5396b2
JC Zaragoza <jzara3115@gmail.com>
2024-10-17 23:04:39 -0700

    Step 10 - Added new buttons and have descriptions for each button within game

--- src/main.ts
+++ src/main.ts
@@ -7,20 +7,38 @@ let cookRate: number = 1;
 const upgradeCostGrowth: number = 1.15;
 
 interface Item {
-  name: string,
-  cost: number,
-  rate: number
-};
+  name: string;
+  cost: number;
+  rate: number;
+}
 
-const availableItems : Item[] = [
-  {name: "A singular bee, pollinates and supports flower growth. + 1/s: ", cost: 10, rate: 1},
-  {name: "Grandmother, she likes helping through the garden. + 5/s: ", cost: 50, rate: 5},
-  {name: "Botonist, needs these flowers to conduct her research. + 10/s: ", cost: 100, rate: 10},
-  {name: "Research Lab, focused on finding the optimal flower dna for faster growth. + 50/s: ", cost: 500, rate: 50},
-  {name: "Arboretum, absolutely filled with flowers, growing its land for its sole purpose of flowers. + 100/s: ", cost: 1000, rate: 100},
+const availableItems: Item[] = [
+  {
+    name: "A singular bee, pollinates and supports flower growth. + 1/s: ",
+    cost: 10,
+    rate: 1,
+  },
+  {
+    name: "Grandmother, she likes helping through the garden. + 5/s: ",
+    cost: 50,
+    rate: 5,
+  },
+  {
+    name: "Botonist, needs these flowers to conduct her research. + 10/s: ",
+    cost: 100,
+    rate: 10,
+  },
+  {
+    name: "Research Lab, focused on finding the optimal flower dna for faster growth. + 50/s: ",
+    cost: 500,
+    rate: 50,
+  },
+  {
+    name: "Arboretum, absolutely filled with flowers, growing its land for its sole purpose of flowers. + 100/s: ",
+    cost: 1000,
+    rate: 100,
+  },
 ];
-
-
 
 const gameName = "Flowers!!!";
 document.title = gameName;
@@ -33,7 +51,7 @@ const mybutton = document.createElement("button");
 const counter = document.createElement("div");
 
 mybutton.textContent = "🌸";
-
+ 
 app.append(mybutton);
 app.append(counter);
 
@@ -48,12 +66,13 @@ function addRate() {
 
 setInterval(addRate, 1000);
 
-for(let i:number = 0; i < availableItems.length; i++){
+for (let i: number = 0; i < availableItems.length; i++) {
   const newButton = document.createElement("button");
   app.append(newButton);
-  newButton.textContent = availableItems[i].name + `${Math.floor(availableItems[i].cost)}`;
+  newButton.textContent =
+    availableItems[i].name + `${Math.floor(availableItems[i].cost)}`;
 
-  if (cookedNum < availableItems[i].cost){
+  if (cookedNum < availableItems[i].cost) {
     newButton.disabled = true;
   }
 
@@ -61,11 +80,12 @@ for(let i:number = 0; i < availableItems.length; i++){
     cookRate = cookRate + availableItems[i].rate;
     cookedNum = cookedNum - availableItems[i].cost;
     availableItems[i].cost = availableItems[i].cost * upgradeCostGrowth;
-    newButton.textContent = availableItems[i].name + `${Math.floor(availableItems[i].cost)}`;
+    newButton.textContent =
+      availableItems[i].name + `${Math.floor(availableItems[i].cost)}`;
   });
 
-  function updateButton(){
-    if (cookedNum < availableItems[i].cost){
+  function updateButton() {
+    if (cookedNum < availableItems[i].cost) {
       newButton.disabled = true;
     } else {
       newButton.disabled = false;
